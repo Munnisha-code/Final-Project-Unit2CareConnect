ac3042a45afb37b7240d6b768a62fce9495faac6
Munnisha <munnishav9@gmail.com>
2026-09-08 16:14:34 -0500

    Add user entity and repository.

---
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.careconnect.careconnect.repository;
+
+import com.careconnect.careconnect.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+}
