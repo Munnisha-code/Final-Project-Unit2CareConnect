4f1decaa7900c79b93e6bda0f56a840d151866ec
Munnisha <munnishav9@gmail.com>
2026-09-09 17:15:17 -0500

    Add trusted contact entity model.

---
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/controller/TrustedContactController.java
@@ -0,0 +1,15 @@
+package com.careconnect.careconnect.controller;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+public class TrustedContactController {
+
+
+
+
+}
---
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/TrustedContactRepository.java
@@ -0,0 +1,4 @@
+package com.careconnect.careconnect.repository;
+
+public class TrustedContactRepository {
+}
