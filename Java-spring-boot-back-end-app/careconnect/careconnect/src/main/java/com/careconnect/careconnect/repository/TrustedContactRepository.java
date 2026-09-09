9a56d7c6572cebfee1ceb67ed587d83ca47843c5
Munnisha <munnishav9@gmail.com>
2026-09-09 18:12:29 -0500

    Add TrustedContact repository.

--- Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/TrustedContactRepository.java
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/TrustedContactRepository.java
@@ -1,4 +1,8 @@
 package com.careconnect.careconnect.repository;
 
-public class TrustedContactRepository {
+
+import com.careconnect.careconnect.model.TrustedContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrustedContactRepository extends JpaRepository<TrustedContact, Long>{
 }
