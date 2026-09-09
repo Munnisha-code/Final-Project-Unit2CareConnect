3a2f93b9b65e868cd7c233ed6e310e1ccf50b083
Munnisha <munnishav9@gmail.com>
2026-09-09 18:21:54 -0500

    Add TrustedContact Contoller.

--- Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/controller/TrustedContactController.java
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/controller/TrustedContactController.java
@@ -1,13 +1,23 @@
 package com.careconnect.careconnect.controller;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.careconnect.careconnect.repository.TrustedContactRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/api/trusted-contacts")
+@CrossOrigin
 
 public class TrustedContactController {
+
+    private final TrustedContactRepository repository;
+
+    public TrustedContactController(TrustedContactRepository repository){
+      this.repository = repository;
+    }
 
 
 
