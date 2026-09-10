0a577df4177b7a0a7a21349c888259a906f37655
Munnisha <munnishav9@gmail.com>
2026-09-10 13:35:50 -0500

    Add user lookup by email

--- Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/UserRepository.java
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.careconnect.careconnect.repository;
 import com.careconnect.careconnect.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByEmail(String email);
 }
