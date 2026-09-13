f11cfb7203656251bd070244d71890e2575f3e77
Munnisha <munnishav9@gmail.com>
2026-09-12 22:16:35 -0500

    Add message save and retrieval APIs for trusted contacts.

--- Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/MessageRepository.java
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.careconnect.careconnect.repository;
 
 import com.careconnect.careconnect.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByTrustedContactIdOrderByCreatedAtAsc(Long trustedContactId);
 }
