17daabe9df9e0734c99bf6ae79f2dec1f59a93ed
Munnisha <munnishav9@gmail.com>
2026-09-15 16:47:36 -0500

    Add user-specific trusted contact API and CORS support.

--- Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/model/TrustedContact.java
+++ Java-spring-boot-back-end-app/careconnect/careconnect/src/main/java/com/careconnect/careconnect/model/TrustedContact.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class TrustedContact {
@@ -15,6 +18,12 @@ public class TrustedContact {
     private String name;
     private String mobileNumber;
     private String relationship;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+
+    private User user;
 
     public TrustedContact(){
 
@@ -27,34 +36,50 @@ public class TrustedContact {
     }
 
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getMobileNumber() {
+
         return mobileNumber;
     }
 
     public void setMobileNumber(String mobileNumber) {
+
         this.mobileNumber = mobileNumber;
     }
 
     public String getRelationship() {
+
         return relationship;
     }
 
     public void setRelationship(String relationship) {
+
         this.relationship = relationship;
     }
+
+    public User getUser() {
+
+        return user; }
+
+    public void setUser(User user){
+
+        this.user = user; }
 }
